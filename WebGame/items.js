6b9c427b753994851e4d7d889a0944da1dc8a742
Filip <145655907+FilipSpektra@users.noreply.github.com>
2024-10-18 01:58:58 +0100

    added files

---
+++ WebGame/items.js
@@ -0,0 +1,69 @@
+//Player Stats
+let radiation = 0
+let health = 100 - raditation
+let armor = 0
+let rad_res = 0
+let fire_res = 0
+let pois_res = 0
+
+//Negetive effects
+let bleeding = false
+let bldg_dur = 0
+
+let poison = false
+let psn_dur = 0
+
+let on_fire = false
+let fire_dur = 0
+
+//S.P.E.C.I.A.L
+let strength = 5
+let perception = 5
+let endurance = 5
+let charisma = 5
+let intelligance = 5
+let agility = 5
+let luck = 5
+
+//Healing
+let bandage = 0
+let stimpA = 0
+let radA = 0
+let radX = 0
+
+//Melee Weapons
+let baseBat = false
+let kitchKnife = false
+
+//Guns
+let tenMM = false
+let tenMM_ammo = 0
+
+let tGun = false
+let tGun_ammo = 0
+
+let leverA_rifle = false
+let lA_rifle_ammo = 0
+
+//Tools
+let lockpick = 0
+let axe = false
+let pickaxe = false
+
+//Quest
+let arm_key = false
+
+//Matarials
+let metal = 0
+let wood = 0
+let plastic = 0
+let fabric = 0
+let glass = 0
+let stone = 0
+let rubber = 0
+let adhesieve = 0
+let oil = 0
+let paper = 0
+
+//Misc
+let arm_loot = false
